1abf90ab2761e104f2fb95ce4baefd9cec4246a6
George <cubcamper@gmail.com>
2017-11-16 10:25:33 -0600

    o

---
+++ Lab12Patterson/src/BinaryTrees/MyDriverClass.java
@@ -0,0 +1,22 @@
+package BinaryTrees;
+
+import java.io.*;
+
+public class MyDriverClass {
+
+	//read Each Student from Student.in
+	//store each student as Student or Graduate Student record
+	
+	//store all the records in a BST
+	
+	
+	//use TreeIterator and a loop to print the students in order of their GPA
+	
+	//Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
